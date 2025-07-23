421a7d33419cfb053503e219eb3338447415aa5c
Annapaneni Bhajarangh <bannuannapaneni3789@gmail.com>
2025-07-23 11:15:31 +0530

    RelationalOperators.dart

---
+++ RelationalOperators.dart
@@ -0,0 +1,17 @@
+void main()
+{
+        int a = 2;
+        int b = 3;
+    
+        print(a > b);  // false
+    
+        print(a < b);  // true
+
+        print(a <= b); // true
+    
+        print(a >= b); // false
+
+        print(a == b);  // false
+    
+        print( a!= b);  // true
+}
