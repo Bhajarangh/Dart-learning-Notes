21ab9d004ee4669365befe16e423ea3cce9a6bea
Annapaneni Bhajarangh <bannuannapaneni3789@gmail.com>
2025-07-23 11:13:52 +0530

    ArithmeticOperators.dart

---
+++ ArithmeticOperators.dart
@@ -0,0 +1,20 @@
+void main()
+{
+    int a = 10;
+    int b = 20;
+    
+    print("sum = ${a+b}");  // Addition
+    print("sub = ${a-b}");  // Subtraction
+    print("div = ${a/b}");  // Division
+    print("mult = ${a*b}");  // Multiply
+    print("reminder = ${a%b}");  // Modulus
+
+}
+/* 
+output:
+sum = 30
+sub = -10
+div = 0.5
+mult = 200
+reminder = 10 
+*/
