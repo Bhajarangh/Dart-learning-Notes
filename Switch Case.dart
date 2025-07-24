fee906f66b36213065a90ef2aabb117169b770d4
Annapaneni Bhajarangh <bannuannapaneni3789@gmail.com>
2025-07-24 09:04:05 +0530

    Switch Case.dart

---
+++ Switch Case.dart
@@ -0,0 +1,26 @@
+void main()
+    {
+        int n = 2;
+        switch (n)
+        {
+            case 1: {
+                print(" The number is 1");
+            }
+            break;
+            
+            case 2: {
+                print(" The number is 2");
+            }
+            break;
+            
+            case 3: {
+                print(" The number is 3");
+            }
+            break;
+            
+            default: {
+                print("This is default case");
+            }
+            break;
+        }
+    }
